a76dd945d3aa715633db8da1227550341b38be7b
zero86 <katanazero86@gmail.com>
2023-05-08 13:12:43 +0900

    [feature] - Header 컴포넌트 작성 tailwind css 및 daisyUI 참고해서 작성 - icon 사용을 위해, react-icons 모듈 추가

--- my-instagram/README.md
+++ my-instagram/README.md
@@ -14,3 +14,14 @@ typescript: v5.0.4
 tailwindcss: v3.3.2
 daisyui: v2.51.6
 ```
+
+- 기능
+```
+1. 로그인, 로그아웃(로그인은 social login: google)
+2. 로그인 된 사용자는 index 페이지로 이동
+3. 로그인한 사용자의 follwing 사용자 / 로그인한 사용자의 소개 및 링크
+4. card 형태 UI 로 게시글이 나열되어있음
+5. card ui 에는 사용자의 아바타, 사용자의 ID, 사진, 좋아요, 즐겨찾기, 작성시간(상대적인 시간), 내용 및 댓글이 있음
+6. 사진 및 View all comments 를 클릭하면 상세로 이동
+
+```
---
+++ my-instagram/src/components/Header/Header.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+import { AiOutlineHome, AiOutlineSearch } from 'react-icons/ai';
+import { BiMessageSquareAdd } from 'react-icons/bi';
+
+export default function Header() {
+  return (
+    <header className="pt-2">
+      <nav className="navbar">
+        <div className="flex-1">
+          <Link href="/" className="normal-case text-3xl text-semibold">
+            Instagram
+          </Link>
+        </div>
+        <div className="flex-none">
+          <label tabIndex={0} className="pr-2 cursor-pointer">
+            <AiOutlineHome size={24} />
+          </label>
+          <label tabIndex={0} className="px-2 cursor-pointer">
+            <AiOutlineSearch size={24} />
+          </label>
+          <label tabIndex={0} className="px-2 cursor-pointer">
+            <BiMessageSquareAdd size={24} />
+          </label>
+          <div className="mx-2.5 dropdown dropdown-end">
+            <label tabIndex={0} className="avatar cursor-pointer">
+              <div className="w-10 rounded-full ring-2 ring-primary ring-offset-base-100 ring-offset-2">
+                <img src="/imgs/avatar/test.jpg" alt="avatar-img" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-32 border border-slate-200"
+            >
+              <li>
+                <a>My Page</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
