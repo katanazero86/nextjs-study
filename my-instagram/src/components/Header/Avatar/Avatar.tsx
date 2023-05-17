8b5c0e239037a54ee1c2c31060b7f5d4407d4d41
zero86 <katanazero86@gmail.com>
2023-05-18 01:40:36 +0900

    [feature] - Avatar.tsx 컴포넌트 부분 코드 수정 img 태그에 width height 기본값 지정

--- my-instagram/src/components/Header/Avatar/Avatar.tsx
+++ my-instagram/src/components/Header/Avatar/Avatar.tsx
@@ -3,10 +3,11 @@ interface AvatarProps {
 }
 
 export default function Avatar({ imgUrl = '' }: AvatarProps) {
+  console.log(imgUrl);
   return (
     <label tabIndex={0} className="avatar cursor-pointer">
       <div className="w-10 rounded-full ring-2 ring-primary ring-offset-base-100 ring-offset-2">
-        <img src={imgUrl} alt="avatar-img" />
+        <img src={imgUrl} alt="avatar-img" width={40} height={40} />
       </div>
     </label>
   );
